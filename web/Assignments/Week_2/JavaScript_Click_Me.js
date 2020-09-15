15e546a02d5e6325399c92fcf0ec3cd8c24666c7
Emiliano Massai <emilianomassai@icloud.com>
2020-09-15 13:07:06 +0200

    started stretch challenge

--- web/Assignments/Week_2/JavaScript_Click_Me.js
+++ web/Assignments/Week_2/JavaScript_Click_Me.js
@@ -1,5 +1,5 @@
 // Function to alert the text "Clicked!" when the "Click Me" button is clicked.
-function clikedAllert() {
+function clickedAlert() {
   window.alert("Clicked!");
 }
 
@@ -14,42 +14,63 @@ function colorValidating() {
     case "Red":
       document.getElementById("select_message").innerHTML = "You selected Red!";
       document.getElementById("first_div").style.color = "red";
+
+      //jQuery used to change the background of the second div
+      $("#second_div").css("background-color", "red");
       break;
 
     case "Green":
       document.getElementById("select_message").innerHTML =
         "You selected Green!";
       document.getElementById("first_div").style.color = "green";
+
+      //jQuery used to change the background of the second div
+      $("#second_div").css("background-color", "green");
       break;
 
     case "Blue":
       document.getElementById("select_message").innerHTML =
         "You selected Blue!";
       document.getElementById("first_div").style.color = "blue";
+
+      //jQuery used to change the background of the second div
+      $("#second_div").css("background-color", "blue");
       break;
 
     case "Yellow":
       document.getElementById("select_message").innerHTML =
         "You selected Yellow!";
       document.getElementById("first_div").style.color = "yellow";
+
+      //jQuery used to change the background of the second div
+      $("#second_div").css("background-color", "yellow");
       break;
 
     case "Brown":
       document.getElementById("select_message").innerHTML =
         "You selected Brown!";
       document.getElementById("first_div").style.color = "brown";
+
+      //jQuery used to change the background of the second div
+      $("#second_div").css("background-color", "brown");
       break;
 
     case "White":
       document.getElementById("select_message").innerHTML =
         "You selected White!";
       document.getElementById("first_div").style.color = "white";
+
+      //jQuery used to change the background of the second div
+      $("#second_div").css("background-color", "white");
       break;
 
     case "Black":
       document.getElementById("select_message").innerHTML =
         "You selected Black!";
       document.getElementById("first_div").style.color = "black";
+
+      //jQuery used to change the background of the second div
+      $("#second_div").css("background-color", "black");
       break;
 
     default:
@@ -58,8 +79,6 @@ function colorValidating() {
   }
 }
 
-/* function hintMessage() {
-  document.getElementById("select_message").innerHTML =
-    "Select between Red, Green, Blue, Yellow or Black.";
+function fadeInOut() {
+  $("#third_div").fadeToggle(1000);
 }
-*/
