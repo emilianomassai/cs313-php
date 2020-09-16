a7f36f19e1db6ca03a0e615906d17d26ab42dc1c
Emiliano Massai <emilianomassai@icloud.com>
2020-09-16 10:09:01 +0200

    Changed few things after looking at the solution

--- web/Assignments/Week_2/JavaScript_Click_Me.js
+++ web/Assignments/Week_2/JavaScript_Click_Me.js
@@ -3,6 +3,10 @@ function clickedAlert() {
   window.alert("Clicked!");
 }
 
+//CHANGED AFTER LOOKING AT THE SOLUTION. In the colorValidating() function, I
+//used a switch statement to check for the prompted values, but in the solution
+//I found a better way.
+
 function colorValidating() {
   //check the input from the user. Change the color of the first div based on
   //what color has been selected. Display an error message if the color is not
@@ -10,73 +14,76 @@ function colorValidating() {
 
   let colorFromUser = document.getElementById("input_color").value;
 
-  switch (colorFromUser) {
-    case "Red":
-      document.getElementById("select_message").innerHTML = "You selected Red!";
-      document.getElementById("first_div").style.color = "red";
+  document.getElementById("first_div").style.color = colorFromUser;
+  $("#second_div").css("background-color", colorFromUser);
 
-      //jQuery used to change the background of the second div
-      $("#second_div").css("background-color", "red");
-      break;
+  // switch (colorFromUser) {
+  //   case "Red":
+  //     document.getElementById("select_message").innerHTML = "You selected Red!";
+  //     document.getElementById("first_div").style.color = "red";
 
-    case "Green":
-      document.getElementById("select_message").innerHTML =
-        "You selected Green!";
-      document.getElementById("first_div").style.color = "green";
+  //     //jQuery used to change the background of the second div
+  //     $("#second_div").css("background-color", "red");
+  //     break;
 
-      //jQuery used to change the background of the second div
-      $("#second_div").css("background-color", "green");
-      break;
+  //   case "Green":
+  //     document.getElementById("select_message").innerHTML =
+  //       "You selected Green!";
+  //     document.getElementById("first_div").style.color = "green";
 
-    case "Blue":
-      document.getElementById("select_message").innerHTML =
-        "You selected Blue!";
-      document.getElementById("first_div").style.color = "blue";
+  //     //jQuery used to change the background of the second div
+  //     $("#second_div").css("background-color", "green");
+  //     break;
 
-      //jQuery used to change the background of the second div
-      $("#second_div").css("background-color", "blue");
-      break;
+  //   case "Blue":
+  //     document.getElementById("select_message").innerHTML =
+  //       "You selected Blue!";
+  //     document.getElementById("first_div").style.color = "blue";
 
-    case "Yellow":
-      document.getElementById("select_message").innerHTML =
-        "You selected Yellow!";
-      document.getElementById("first_div").style.color = "yellow";
+  //     //jQuery used to change the background of the second div
+  //     $("#second_div").css("background-color", "blue");
+  //     break;
 
-      //jQuery used to change the background of the second div
-      $("#second_div").css("background-color", "yellow");
-      break;
+  //   case "Yellow":
+  //     document.getElementById("select_message").innerHTML =
+  //       "You selected Yellow!";
+  //     document.getElementById("first_div").style.color = "yellow";
 
-    case "Brown":
-      document.getElementById("select_message").innerHTML =
-        "You selected Brown!";
-      document.getElementById("first_div").style.color = "brown";
+  //     //jQuery used to change the background of the second div
+  //     $("#second_div").css("background-color", "yellow");
+  //     break;
 
-      //jQuery used to change the background of the second div
-      $("#second_div").css("background-color", "brown");
-      break;
+  //   case "Brown":
+  //     document.getElementById("select_message").innerHTML =
+  //       "You selected Brown!";
+  //     document.getElementById("first_div").style.color = "brown";
 
-    case "White":
-      document.getElementById("select_message").innerHTML =
-        "You selected White!";
-      document.getElementById("first_div").style.color = "white";
+  //     //jQuery used to change the background of the second div
+  //     $("#second_div").css("background-color", "brown");
+  //     break;
 
-      //jQuery used to change the background of the second div
-      $("#second_div").css("background-color", "white");
-      break;
+  //   case "White":
+  //     document.getElementById("select_message").innerHTML =
+  //       "You selected White!";
+  //     document.getElementById("first_div").style.color = "white";
 
-    case "Black":
-      document.getElementById("select_message").innerHTML =
-        "You selected Black!";
-      document.getElementById("first_div").style.color = "black";
+  //     //jQuery used to change the background of the second div
+  //     $("#second_div").css("background-color", "white");
+  //     break;
 
-      //jQuery used to change the background of the second div
-      $("#second_div").css("background-color", "black");
-      break;
+  //   case "Black":
+  //     document.getElementById("select_message").innerHTML =
+  //       "You selected Black!";
+  //     document.getElementById("first_div").style.color = "black";
 
-    default:
-      document.getElementById("select_message").innerHTML =
-        "Please select between Red, Green, Blue, Yellow, Brown, White or Black. It is case sensitive!";
-  }
+  //     //jQuery used to change the background of the second div
+  //     $("#second_div").css("background-color", "black");
+  //     break;
+
+  //   default:
+  //     document.getElementById("select_message").innerHTML =
+  //       "Please select between Red, Green, Blue, Yellow, Brown, White or Black. It is case sensitive!";
+  // }
 }
 
 function fadeInOut() {
