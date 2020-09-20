c1d9ec09f91c008d5b176b1a1280eb267eb5440a
Emiliano Massai <emilianomassai@icloud.com>
2020-09-20 10:03:50 +0100

    .

--- web/Assignments/Home_Page/Emiliano_Home_Page.php
+++ web/Assignments/Home_Page/Emiliano_Home_Page.php
@@ -18,7 +18,7 @@
 //$today = date("m.d.y");                         // 03.10.01
 //$today = date("j, n, Y");                       // 10, 3, 2001
 //$today = date("Ymd");                           // 20010310
-//$today = date('h-i-s, j-m-y, it is w Day');     // 05-16-18, 10-03-01, 1631 1618 6 Satpm01
+//$today = date('h-i-s, j-m-y, it is w Day');     // 05-16-18, 10-03-01, 1631 1618 6 Sat pm 01
 //$today = date('\i\t \i\s \t\h\e jS \d\a\y.');   // it is the 10th day.
 //$today = date("D M j G:i:s T Y");               // Sat Mar 10 17:16:18 MST 2001
 //$today = date('H:m:s \m \i\s\ \m\o\n\t\h');     // 17:03:18 m is month
