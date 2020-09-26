f3728d5abe8af5d7da372ba9592b4c9237f61f6a
Emiliano Massai <emilianomassai@icloud.com>
2020-09-26 21:17:57 +0100

    .

--- web/Assignments/Week_3/Result_page.php
+++ web/Assignments/Week_3/Result_page.php
@@ -3,13 +3,11 @@
 <body>
 
     Welcome <?php echo $_POST["full_name"]; ?><br>
-    Your email address is: <?php echo $_POST["email"]; ?><br>
+    Your email address is: <a href="mailto:<?php echo $to; ?> <?php echo $_POST["email"]; ?></a><br>
 
 
-    echo "<a href='email'>";
-
-        Your major is: <?php echo $_POST["major"]; ?><br>
-        Your comments are: <?php echo $_POST["comments"]; ?><br>
+    Your major is: <?php echo $_POST["major"]; ?><br>
+    Your comments are: <?php echo $_POST["comments"]; ?><br>
 
 
 </body>
