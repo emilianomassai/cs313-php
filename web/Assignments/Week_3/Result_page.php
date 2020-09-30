39343fc7182c0040a48dbe3c71afc01c3261486c
Emiliano Massai <emilianomassai@icloud.com>
2020-09-30 13:43:24 +0100

    .

--- web/Assignments/Week_3/Result_page.php
+++ web/Assignments/Week_3/Result_page.php
@@ -9,12 +9,10 @@
     Your comments are: <?php echo $_POST["comments"]; ?><br>
 
     You visited <?php
-if (isset($_POST['submit'])) { //to run PHP script on submit
-    if (!empty($_POST['cont_list'])) {
+if (!empty($_POST['cont_list'])) {
 // Loop to store and display values of individual checked checkbox.
-        foreach ($_POST['cont_list'] as $selected) {
-            echo $selected . "</br>";
-        }
+    foreach ($_POST['cont_list'] as $selected) {
+        echo $selected . "</br>";
     }
 }
 ?>
