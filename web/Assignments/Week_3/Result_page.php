cb737de97401345ffc05e73654b4937a7c4316fa
Emiliano Massai <emilianomassai@icloud.com>
2020-09-30 13:49:12 +0100

    .

--- web/Assignments/Week_3/Result_page.php
+++ web/Assignments/Week_3/Result_page.php
@@ -12,7 +12,7 @@
 if (!empty($_POST['cont_list'])) {
 // Loop to store and display values of individual checked checkbox.
     foreach ($_POST['cont_list'] as $selected) {
-        if ($selected == $lastElement) {
+        if ($selected == end($selected)) {
             echo $selected . ".";
         } else {
             echo $selected . ", ";
