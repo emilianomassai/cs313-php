5ff5f6dd378ef8c72a7ee267d5abb52ece546e36
Emiliano Massai <emilianomassai@icloud.com>
2020-09-30 13:41:17 +0100

    .

--- web/Assignments/Week_3/Result_page.php
+++ web/Assignments/Week_3/Result_page.php
@@ -8,7 +8,16 @@
     Your major is: <?php echo $_POST["major"]; ?><br>
     Your comments are: <?php echo $_POST["comments"]; ?><br>
 
-    You visited <?php echo $_POST["cont"]; ?>
+    You visited <?php
+if (isset($_POST['submit'])) { //to run PHP script on submit
+    if (!empty($_POST['cont_list'])) {
+// Loop to store and display values of individual checked checkbox.
+        foreach ($_POST['cont_list'] as $selected) {
+            echo $selected . "</br>";
+        }
+    }
+}
+?>
 
 </body>
 
