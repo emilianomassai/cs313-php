ad368313b2df1e2920c5d50ded107bb55cd3a64f
Emiliano Massai <emilianomassai@icloud.com>
2020-09-30 17:31:33 +0100

    .

--- web/Assignments/Week_3/Result_page.php
+++ web/Assignments/Week_3/Result_page.php
@@ -14,33 +14,34 @@ if (!empty($continents_Array)) {
 // Loop to store and display values of individual checked checkbox.
 
     foreach ($continents_Array as $selected => $element) {
+        switch ($selected) {
+            case NA:
+                echo "North America";
+                break;
+            case SA:
+                echo "South America";
+                break;
+            case EU:
+                echo "Europe";
+                break;
+            case AI:
+                echo "Asia";
+                break;
+            case AU:
+                echo "Australia";
+                break;
+            case AF:
+                echo "Africa";
+                break;
+            case AN:
+                echo "Antarctica";
+                break;
+        }
 
         // if the checked value is the last, add a period instead of a comma.
 
         if ($selected === array_key_last($continents_Array)) {
-            switch ($selected) {
-                case NA:
-                    echo "North America";
-                    break;
-                case SA:
-                    echo "South America";
-                    break;
-                case EU:
-                    echo "Europe";
-                    break;
-                case AI:
-                    echo "Asia";
-                    break;
-                case AU:
-                    echo "Australia";
-                    break;
-                case AF:
-                    echo "Africa";
-                    break;
-                case AN:
-                    echo "Antarctica";
-                    break;
-            }
+
             echo '.';
         } else {
             echo ', ';
