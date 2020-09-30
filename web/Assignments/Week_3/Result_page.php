13f556bb0c406ce3162bd42de7b94c7be8c64a11
Emiliano Massai <emilianomassai@icloud.com>
2020-09-30 15:24:54 +0100

    .

--- web/Assignments/Week_3/Result_page.php
+++ web/Assignments/Week_3/Result_page.php
@@ -38,19 +38,15 @@ if (!empty($_POST['cont_list'])) {
                 break;
         }
 
-        if (end($_POST['cont_list'])) {
-            echo ".";
-        } else {
-            echo ", ";
-        }
+        echo ", ";
 
     }
 
     // if the checked value is the last, add a period instead of a comma.
 
 }
-// if (array_key_last($selected)) {
-//     echo ".";}
+if (array_key_last($selected)) {
+    echo ".";}
 
 ?>
 
