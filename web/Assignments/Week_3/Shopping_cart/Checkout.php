15d6adaa154acd2cfbda8148e111cd6d38c77ad0
Emiliano Massai <emilianomassai@icloud.com>
2020-10-01 23:07:57 +0100

    .

--- web/Assignments/Week_3/Shopping_cart/Checkout.php
+++ web/Assignments/Week_3/Shopping_cart/Checkout.php
@@ -39,11 +39,11 @@ session_start();
         <!-- TOP MENU -->
 
         <div class="navbar">
-            <a href="../Shopping_cart/Browse_Items.php">Return to browse items page</a>
+            <a href="../Shopping_cart/Browse_Items.php">Edit your cart <img src="../Shopping_cart/images/cart.png"
+                    alt="cart" /></a>
             <p></p>
-            <a href="../Shopping_cart/View_Cart.php">Return to the cart
-                <img src="../Shopping_cart/images/cart.png" alt="cart" />
-            </a>
+            <p></p>
+
         </div>
     </div>
 
