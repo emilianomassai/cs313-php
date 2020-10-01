61deadb57d8abaa80823ce8b130e5dafc9057fe6
Emiliano Massai <emilianomassai@icloud.com>
2020-10-01 11:31:10 +0100

    .

--- web/Assignments/Week_3/Shopping_cart/View_Cart.php
+++ web/Assignments/Week_3/Shopping_cart/View_Cart.php
@@ -41,7 +41,7 @@ session_start();
         <div class="navbar">
             <a href="../Shopping_cart/Browse_Items.php">Return to browse items page</a>
             <p></p>
-            <a href="../Shopping_cart/Checkout.php" id="checkout_btn_id">Checkout
+            <a action="Checkout.php" id="checkout_btn_id" type="submit">Checkout
                 <img src="../Shopping_cart/images/cart.png" alt="cart" />
             </a>
         </div>
