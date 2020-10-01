b27cbbb658bcb64c3449e09098b7dedfd03a8fc0
Emiliano Massai <emilianomassai@icloud.com>
2020-10-01 17:43:51 +0100

    .

--- web/Assignments/Week_3/Shopping_cart/Checkout.php
+++ web/Assignments/Week_3/Shopping_cart/Checkout.php
@@ -147,7 +147,6 @@ $sum = 0;
             <?php
 //store the total of the items in a variable
 //Make sure that the session variable actually exists!
-$sum = 0;
 
 if (isset($_SESSION['cart'])) {
 
@@ -156,7 +155,8 @@ if (isset($_SESSION['cart'])) {
         //sum up the total of all the items in the cart
         $sum += $productPrice;
     }
-    session_reset();
+    $sum = 0;
+
 }
 ?>
 
