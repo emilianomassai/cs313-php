b74ca2b42d41dc04e12cf489795e09c7b6654d01
Emiliano Massai <emilianomassai@icloud.com>
2020-10-10 12:44:44 +0100

    .

--- web/Assignments/Week_5/budgetApp.php
+++ web/Assignments/Week_5/budgetApp.php
@@ -52,10 +52,19 @@ echo "<br>";
 echo "The following is  the list of all the users:";
 echo "<br>";
 
-foreach ($db->query('SELECT display_name, transaction.amount, transaction.notes, transaction.category,transaction.date FROM budgetUser') as $row) {
+foreach ($db->query('SELECT display_name FROM budgetUser') as $row) {
     echo $row['display_name'];
-    echo $row['amount'];
     echo '<br/>';
+    foreach ($db->query('SELECT amount, notes, category, date FROM transaction') as $row) {
+        echo $row['amount'];
+        echo '<br/>';
+        echo $row['notes'];
+        echo '<br/>';
+        echo $row['category'];
+        echo '<br/>';
+        echo $row['date'];
+        echo '<br/>';
+    }
 }
 echo '<br/>';
 
