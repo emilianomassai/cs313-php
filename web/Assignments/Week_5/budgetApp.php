ebf3c3f7a266001c7d4fdab58fe4396d06a75df2
Emiliano Massai <emilianomassai@icloud.com>
2020-10-16 20:39:16 +0100

    .

--- web/Assignments/Week_5/budgetApp.php
+++ web/Assignments/Week_5/budgetApp.php
@@ -166,10 +166,8 @@ $total_column = $select->columnCount();
 for ($counter = 0; $counter < $total_column; $counter++) {
     $meta = $select->getColumnMeta($counter);
     $column[] = $meta['name'];?>
-                    <label for="<?php echo $column[$counter] ?>" style="text-align: center;">
-                        <?php echo $column[$counter] ?></label>
-                    <input type="text" id="<?php echo $column[$counter] ?>" name="<?php echo $column[$counter] ?>"
-                        style="text-align: center;"> <br>
+                    <label for="<?php echo $column[$counter] ?>"> <?php echo $column[$counter] ?></label>
+                    <input type="text" id="<?php echo $column[$counter] ?>" name="<?php echo $column[$counter] ?>"> <br>
                     <?php }?>
 
 
