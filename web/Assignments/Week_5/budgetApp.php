dafdc7db0b4f53f26c48701db44d1c928f531253
Emiliano Massai <emilianomassai@icloud.com>
2020-10-16 21:17:34 +0100

    .

--- web/Assignments/Week_5/budgetApp.php
+++ web/Assignments/Week_5/budgetApp.php
@@ -65,6 +65,10 @@ foreach ($db->query('SELECT amount, user_id, notes, category, date FROM transact
 
 ?>
 
+    <div>
+        <img src="../Home_Page/images/me.jpg" alt="budget App Picture" id="budgetAppPicture " />
+    </div>
+
     <h1>Project Title: Budget App </h1>
     <p>Welcome to this budget app! In this page you can see different ways to retrieve information from our database
         about our users. Please choose one option and read the description for more details:</p>
