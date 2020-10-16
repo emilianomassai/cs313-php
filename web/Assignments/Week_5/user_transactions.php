10fbf893868d6d3ae5ab7f5739be6c6d5258e754
Emiliano Massai <emilianomassai@icloud.com>
2020-10-16 21:46:47 +0100

    .

--- web/Assignments/Week_5/user_transactions.php
+++ web/Assignments/Week_5/user_transactions.php
@@ -9,7 +9,7 @@ session_start();
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
     <link rel="stylesheet" href="style.css" />
-
+    <link rel="icon" type="image/ico" href="./BudgetAppImages/budgetAppIcon.png">
     <!--Title in the browser title bar.-->
     <title>CS:313 - Week 5</title>
     <!-- heading of the web page -->
