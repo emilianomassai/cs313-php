c0edd05a1fa7f38caa10264f3bbd9d2b7f0d203a
Emiliano Massai <emilianomassai@icloud.com>
2020-10-19 10:59:29 +0100

    .

--- web/Assignments/Week_6/Team_Activity/enter_scripture.php
+++ web/Assignments/Week_6/Team_Activity/enter_scripture.php
@@ -34,7 +34,7 @@ if (isset($_POST['s'])) {
 
     </form>
     <div>
-        <a href="../web/Assignments/Home_Page/assignments.php" id="CS313_assignments_btn_id">
+        <a href="../../Home_Page/assignments.php" id="CS313_assignments_btn_id">
             Go to the CS313 Assignment's page
         </a>
     </div>
