67446b16939b624b4c08503e1ddbb14dfd104c33
Emiliano Massai <emilianomassai@icloud.com>
2020-10-19 10:37:05 +0100

    .

---
+++ web/Assignments/Week_6/Team_Activity/enter_scripture.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en-US">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+
+
+
+    <div>
+        <a href="../Home_Page/assignments.php" id="CS313_assignments_btn_id">
+            Go to the CS313 Assignment's page
+        </a>
+    </div>
+
+    <footer>
+        <p style="text-align: center;">
+            Copyright © 2020 emiDev Inc. All rights reserved.
+        </p>
+    </footer>
+    </body>
+
+</html>
