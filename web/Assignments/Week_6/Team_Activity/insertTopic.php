74756cc3f5711040f0c36d3975538d70213853e0
Emiliano Massai <emilianomassai@icloud.com>
2020-10-21 10:38:05 +0100

    .

--- web/Assignments/Week_6/Team_Activity/insertTopic.php
+++ web/Assignments/Week_6/Team_Activity/insertTopic.php
@@ -1,87 +1,87 @@
 <?php
-​
-    /*
-        CREATE topic (
-            id SERIAL PRIMARY KEY,
-            name VARCHAR(255)
-        );
-​
-        CREATE scriptures_topic (
-            id SERIAL PRIMARY KEY,
-            scripture_id INT REFERENCES sriptures (id),
-            topic_id INT REFERENCES topic (id)
-        )
-     */
-    require_once "../../../db/connection.php";
-    $db = connectDb();
-​
-    $book = htmlspecialchars($_POST['book']);
-    $chapter = htmlspecialchars($_POST['chapter']);
-    $verse = htmlspecialchars($_POST['verse']);
-    $content = htmlspecialchars($_POST['content']);
-​
-    $topics = array();
-​
-    $queryTopic = "SELECT id, name FROM topic"; 
-    $stmtTopic = $db->prepare($queryTopic);
-    $stmtTopic->execute();
-    $resultTopics = $stmtTopic->fetchAll(PDO::FETCH_ASSOC);
-​
-    $charity = $_POST[''];
-​
-    if ($stmtTopic->rowCount() > 0) {
-        foreach($resultTopics as $topic) {
-            $topicNameId = preg_replace('/\s+/', '', strtolower($topic['name'])) . "_" . $topic['id'];
-            if (isset($_POST[$topicNameId])) {
-                array_push($topics, htmlspecialchars($_POST[$topicNameId]));
-            }            
-        }
-    } else {
-        echo "Topic is empty!";
-        die();
-    }
-​
-    function createScriptureTopic($db, $scriptureId, $topicId) {
-        $insertScriptureTopic = "INSERT INTO scriptures_topic (scripture_id, topic_id)
-            VALUES(:scripture_id, :topic_id)";
-        $insertScriptureTopic = $db->prepare($insertScriptureTopic);
-        $insertScriptureTopic->bindParam(':scripture_id', $scriptureId, PDO::PARAM_INT);
-        $insertScriptureTopic->bindParam(':topic_id', $topicId, PDO::PARAM_INT);
-        $insertScriptureTopic->execute();
-    }
-​
-    // Create scripture
-    $insertScripture = "INSERT INTO scriptures (book, chapter, verse, content)
-        VALUES(:book, :chapter, :verse, :content)";
-    $stmtScripture = $db->prepare($insertScripture);
-    $stmtScripture->bindParam(':book', $book, PDO::PARAM_STR);
-    $stmtScripture->bindParam(':chapter', $chapter, PDO::PARAM_STR);
-    $stmtScripture->bindParam(':verse', $verse, PDO::PARAM_STR);
-    $stmtScripture->bindParam(':content', $content, PDO::PARAM_STR);
-    $stmtScripture->execute();
-    $scriptureId = $db->lastInsertId('scriptures_id_seq');
-​
-    // Link newly created scripture to topic
-    if (count($topics) > 0) {
-        foreach($topics as $topicId) {
-            createScriptureTopic($db, $scriptureId, $topicId);
-        }
-    } else {
-        echo "No topic selected!";
-    }
-​
-    if (isset($_POST['add_topic'])) {
-        $topicName = htmlspecialchars($_POST['topic_name']);
-        // Create topic
-        $topicName = htmlspecialchars($_POST['topic_name']);
-        $insertTopic = "INSERT INTO topic (name) VALUES(:name)";
-        $stmtTopic = $db->prepare($insertTopic);
-        $stmtTopic->bindParam(':name', $topicName, PDO::PARAM_STR);
-        $stmtTopic->execute();
-        $topicId = $db->lastInsertId('topic_id_seq');
-        createScriptureTopic($db, $scriptureId, $topicId);   
-    }
-    
-    header("Location: insert_scripture.php");
-    die();
+/**********************************************************
+* File: insertTopic.php
+* Author: Br. Burton
+* 
+* Description: Takes input posted from topicEntry.php
+*   This file enters a new scripture into the database
+*   along with its associated topics.
+*
+*   This file does NOT do any rendering at all,
+*   instead it redirects the user to showTopics.php to see
+*   the resulting list.
+***********************************************************/
+
+// get the data from the POST
+$book = $_POST['txtBook'];
+$chapter = $_POST['txtChapter'];
+$verse = $_POST['txtVerse'];
+$content = $_POST['txtContent'];
+$topicIds = $_POST['chkTopics'];
+
+// For debugging purposes, you might include some echo statements like this
+// and then not automatically redirect until you have everything working.
+
+// echo "book=$book\n";
+// echo "chapter=$chapter\n";
+// echo "verse=$verse\n";
+// echo "content=$content\n";
+
+// we could (and should!) put additional checks here to verify that all this data is actually provided
+
+
+require("dbConnect.php");
+$db = get_db();
+
+try
+{
+	// Add the Scripture
+
+	// We do this by preparing the query with placeholder values
+	$query = 'INSERT INTO scripture(book, chapter, verse, content) VALUES(:book, :chapter, :verse, :content)';
+	$statement = $db->prepare($query);
+
+	// Now we bind the values to the placeholders. This does some nice things
+	// including sanitizing the input with regard to sql commands.
+	$statement->bindValue(':book', $book);
+	$statement->bindValue(':chapter', $chapter);
+	$statement->bindValue(':verse', $verse);
+	$statement->bindValue(':content', $content);
+
+	$statement->execute();
+
+	// get the new id
+	$scriptureId = $db->lastInsertId("scripture_id_seq");
+
+	// Now go through each topic id in the list from the user's checkboxes
+	foreach ($topicIds as $topicId)
+	{
+		echo "ScriptureId: $scriptureId, topicId: $topicId";
+
+		// Again, first prepare the statement
+		$statement = $db->prepare('INSERT INTO scripture_topic(scriptureId, topicId) VALUES(:scriptureId, :topicId)');
+
+		// Then, bind the values
+		$statement->bindValue(':scriptureId', $scriptureId);
+		$statement->bindValue(':topicId', $topicId);
+
+		$statement->execute();
+	}
+}
+catch (Exception $ex)
+{
+	// Please be aware that you don't want to output the Exception message in
+	// a production environment
+	echo "Error with DB. Details: $ex";
+	die();
+}
+
+// finally, redirect them to a new page to actually show the topics
+header("Location: showTopics.php");
+
+die(); // we always include a die after redirects. In this case, there would be no
+       // harm if the user got the rest of the page, because there is nothing else
+       // but in general, there could be things after here that we don't want them
+       // to see.
+
 ?>
