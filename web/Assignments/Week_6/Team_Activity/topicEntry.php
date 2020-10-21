739fa38315da8452d4c1bfa2dd8a239f0959b9fe
Emiliano Massai <emilianomassai@icloud.com>
2020-10-21 23:40:20 +0100

    .

--- web/Assignments/Week_6/Team_Activity/topicEntry.php
+++ web/Assignments/Week_6/Team_Activity/topicEntry.php
@@ -101,7 +101,7 @@ try
 
     </div>
     <div>
-        <a href="../../Home_Page/assignments.php" id="CS313_assignments_btn_id">
+        <a href="/web/Assignments/Home_Page/assignments.php" id="CS313_assignments_btn_id">
             Go to the CS313 Assignment's page
         </a>
     </div>
