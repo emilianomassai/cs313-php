7c9c43e3d2c022b094e405abdec8c08b5a12b630
Emiliano Massai <emilianomassai@icloud.com>
2020-10-22 14:58:25 +0100

    .

--- web/Assignments/Week_6/Team_Activity/topicEntry.php
+++ web/Assignments/Week_6/Team_Activity/topicEntry.php
@@ -101,6 +101,17 @@ try
 
     </div>
 
+    <div>
+        <a href="../../Home_Page/assignments.php" id="CS313_assignments_btn_id">
+            Go to the CS313 Assignment's page
+        </a>
+    </div>
+
+    <footer>
+        <p style="text-align: center;">
+            Copyright © 2020 emiDev Inc. All rights reserved.
+        </p>
+    </footer>
 </body>
 
 </html>
