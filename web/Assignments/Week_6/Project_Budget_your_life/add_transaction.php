f4cfd1975f541989d465a3c3ab079c8684e97e1f
Emiliano Massai <emilianomassai@icloud.com>
2020-10-22 20:29:35 +0100

    .

--- web/Assignments/Week_6/Project_Budget_your_life/add_transaction.php
+++ web/Assignments/Week_6/Project_Budget_your_life/add_transaction.php
@@ -8,6 +8,28 @@
  *   along with its associated date, amount, user_id, notes and category.
  *
  ***********************************************************/
+?>
+
+<!DOCTYPE html>
+<html lang="en-US">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <link rel="stylesheet" href="style.css" />
+    <link rel="icon" type="image/ico" href="../Project_Budget_your_life/BudgetAppImages/budgetAppIcon.png">
+
+    <script src="../Project_Budget_your_life/javaScript.js"></script>
+
+    <!--Title in the browser title bar.-->
+    <title>Budget Your Life</title>
+    <!-- heading of the web page -->
+</head>
+
+<body>
+
+    <?php
 
 // start session
 session_start();
@@ -30,67 +52,54 @@ foreach ($db->query('SELECT display_name, user_name, user_id, password FROM budg
     ];
     $_SESSION['users'] = $users_array;
 
-    if ($_POST["userID"] == $users_array[$count]['user_id']) {
+    // Here I should have a form with all the transaction information such as:
 
-        echo 'Name: ' . $users_array[$count]['display_name'] . ';';
-        echo '<br>';
-        echo 'User ID: ' . $users_array[$count]['user_id'] . ';';
-        echo '<br>';
-        echo 'Username: ' . $users_array[$count]['user_name'] . ';';
-        echo '<br>';
-        echo 'Password: ' . $users_array[$count]['password'] . '.';
-        echo '<br>';
+// user_id
+    // amount
+    // notes
+    // category
+    // date (try to add dynamic date with PHP)
+
+    if ($_POST["userID"] == $users_array[$count]['user_id']) {?>
+
+    <h1>Ready to add a transaction, <?php echo $users_array[$count]['display_name'] ?>?</h1>
+    <p>Please add all the following details:</p>
+
+    <!-- use POST to link the current user to the new transaction and add it to the database -->
+
+    <h3>Hello <?php echo $users_array[$count]['display_name'] ?>, your id is
+        <?php echo $users_array[$count]['user_id'] ?></h3>
+
+    <?php
+$actualUserId = $users_array[$count]['user_id'];
+
+// echo 'Name: ' . $users_array[$count]['display_name'] . ';';
+        // echo '<br>';
+        // echo 'User ID: ' . $users_array[$count]['user_id'] . ';';
+        // echo '<br>';
+        // echo 'Username: ' . $users_array[$count]['user_name'] . ';';
+        // echo '<br>';
+        // echo 'Password: ' . $users_array[$count]['password'] . '.';
+        // echo '<br>';
+        $query = 'INSERT INTO public.transaction(user_id, amount, notes, category, date) VALUES(:user_id, :amount, :notes, :category, :date)';
+        $statement = $db->prepare($query);
+
+// Now we bind the values to the placeholders. This does some nice things
+        // including sanitizing the input with regard to sql commands.
+        $statement->bindValue(':user_id', $actualUserId);
+        $statement->bindValue(':amount', $amount);
+        $statement->bindValue(':notes', $notes);
+        $statement->bindValue(':category', $category);
+        $statement->bindValue(':date', $date);
+
+        $statement->execute();
 
     }
     $count++;
 }
 ?>
 
-<head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <link rel="stylesheet" href="style.css" />
-    <link rel="icon" type="image/ico" href="../Project_Budget_your_life/BudgetAppImages/budgetAppIcon.png">
 
-    <script src="../Project_Budget_your_life/javaScript.js"></script>
-
-    <!--Title in the browser title bar.-->
-    <title>Budget Your Life</title>
-    <!-- heading of the web page -->
-</head>
-
-<body>
-
-    <h1>Ready to add a transaction, <?php echo $display_name ?>?</h1>
-    <p>Please add all the following details:</p>
-
-    <!-- use POST to link the current user to the new transaction and add it to the database -->
-
-    <h3>Hello <?php echo $display_name ?>, your id is <?php echo $user_id ?></h3>
-    <?php
-// Here I should have a form with all the transaction information such as:
-
-// user_id
-// amount
-// notes
-// category
-// date (try to add dynamic date with PHP)
-
-$query = 'INSERT INTO public.transaction(user_id, amount, notes, category, date) VALUES(:user_id, :amount, :notes, :category, :date)';
-$statement = $db->prepare($query);
-
-// Now we bind the values to the placeholders. This does some nice things
-// including sanitizing the input with regard to sql commands.
-$statement->bindValue(':user_id', $user_id);
-$statement->bindValue(':amount', $amount);
-$statement->bindValue(':notes', $notes);
-$statement->bindValue(':category', $category);
-$statement->bindValue(':date', $date);
-
-$statement->execute();
-
-?>
 
     <div>
         <a href="../Project_Budget_your_life/budgetApp.php" id="CS313_assignments_btn_id">
