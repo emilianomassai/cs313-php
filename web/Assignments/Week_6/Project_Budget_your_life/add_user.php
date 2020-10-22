cb430592498736ce36ca177785aa868cb56b2cd9
Emiliano Massai <emilianomassai@icloud.com>
2020-10-22 17:59:14 +0100

    .

--- web/Assignments/Week_6/Project_Budget_your_life/add_user.php
+++ web/Assignments/Week_6/Project_Budget_your_life/add_user.php
@@ -14,7 +14,44 @@
 $display_name = $_POST['display_name'];
 $user_name = $_POST['user_name'];
 $password = $_POST['password'];
+?>
 
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <link rel="stylesheet" href="style.css" />
+    <link rel="icon" type="image/ico" href="../Project_Budget_your_life/BudgetAppImages/budgetAppIcon.png">
+
+    <script src="../Project_Budget_your_life/javaScript.js"></script>
+
+    <!--Title in the browser title bar.-->
+    <title>Budget Your Life</title>
+    <!-- heading of the web page -->
+</head>
+
+<body>
+
+    <h1>Welcome, <?php $display_name?>!</h1>
+    <p>Please select an option to create your budget:</p>
+
+    <div>
+        <a href="../Project_Budget_your_life/budgetApp.php" id="CS313_assignments_btn_id">
+            Go back to the App Homepage
+        </a>
+    </div>
+
+    <footer>
+        <p style="text-align: center;">
+            Copyright © <?php echo $today = date("Y"); ?> emiDev Inc. All rights reserved.
+        </p>
+    </footer>
+</body>
+
+</html>
+
+<?php
 require "../Project_Budget_your_life/connectAppDB.php";
 $db = get_db();
 try
@@ -65,37 +102,3 @@ die(); // we always include a die after redirects. In this case, there would be 
 // to see.
 
 ?>
-
-<head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <link rel="stylesheet" href="style.css" />
-    <link rel="icon" type="image/ico" href="../Project_Budget_your_life/BudgetAppImages/budgetAppIcon.png">
-
-    <script src="../Project_Budget_your_life/javaScript.js"></script>
-
-    <!--Title in the browser title bar.-->
-    <title>Budget Your Life</title>
-    <!-- heading of the web page -->
-</head>
-
-<body>
-
-    <h1>Welcome, <?php $display_name?>!</h1>
-    <p>Please select an option to create your budget:</p>
-
-    <div>
-        <a href="../Project_Budget_your_life/budgetApp.php" id="CS313_assignments_btn_id">
-            Go back to the App Homepage
-        </a>
-    </div>
-
-    <footer>
-        <p style="text-align: center;">
-            Copyright © <?php echo $today = date("Y"); ?> emiDev Inc. All rights reserved.
-        </p>
-    </footer>
-</body>
-
-</html>
