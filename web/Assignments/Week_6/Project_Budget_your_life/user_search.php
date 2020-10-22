17c5adbbf39a45ad31845c18fa9495042fac1c39
Emiliano Massai <emilianomassai@icloud.com>
2020-10-22 16:32:57 +0100

    .

--- web/Assignments/Week_6/Project_Budget_your_life/user_search.php
+++ web/Assignments/Week_6/Project_Budget_your_life/user_search.php
@@ -76,8 +76,8 @@ if ($isUser == false) {
 
 
     <div>
-        <a href="../../Home_Page/assignments.php" id="CS313_assignments_btn_id">
-            Go to the CS313 Assignment's page
+        <a href="../Project_Budget_your_life/budgetApp.php" id="CS313_assignments_btn_id">
+            Go back to the App Homepage
         </a>
     </div>
 
