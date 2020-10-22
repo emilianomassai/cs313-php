bfa01848ecf088148dbcd17753adb8f96e2a6ec1
Emiliano Massai <emilianomassai@icloud.com>
2020-10-22 17:11:07 +0100

    Add user to DB from php page

--- web/Assignments/Week_6/Project_Budget_your_life/add_user.php
+++ web/Assignments/Week_6/Project_Budget_your_life/add_user.php
@@ -21,3 +21,53 @@ $password = $_POST['password'];
 echo "display_name=$display_name\n";
 echo "user_name=$user_name\n";
 echo "password=$password\n";
+
+require "../Project_Budget_your_life/connectAppDB.php";
+$db = get_db();
+
+try
+{
+    // Add the user
+
+    // We do this by preparing the query with placeholder values
+    $query = 'INSERT INTO public.budgetUser(display_name, user_name, password) VALUES(:display_name, :user_name, :password)';
+    $statement = $db->prepare($query);
+
+    // Now we bind the values to the placeholders. This does some nice things
+    // including sanitizing the input with regard to sql commands.
+    $statement->bindValue(':display_name', $display_name);
+    $statement->bindValue(':user_name', $user_name);
+    $statement->bindValue(':password', $password);
+
+    $statement->execute();
+
+    // // get the new id
+    // $scriptureId = $db->lastInsertId("public.budgetUser_user_id_seq");
+
+    // // Now go through each topic id in the list from the user's checkboxes
+    // foreach ($topicIds as $topicId) {
+    //     echo "ScriptureId: $scriptureId, topicId: $topicId";
+
+    //     // Again, first prepare the statement
+    //     $statement = $db->prepare('INSERT INTO scripture_topic(scriptureId, topicId) VALUES(:scriptureId, :topicId)');
+
+    //     // Then, bind the values
+    //     $statement->bindValue(':scriptureId', $scriptureId);
+    //     $statement->bindValue(':topicId', $topicId);
+
+    //     $statement->execute();
+    // }
+} catch (Exception $ex) {
+    // Please be aware that you don't want to output the Exception message in
+    // a production environment
+    echo "Error with DB. Details: $ex";
+    die();
+}
+
+// finally, redirect them to a new page to actually show the topics
+header("Location: showTopics.php");
+
+die(); // we always include a die after redirects. In this case, there would be no
+// harm if the user got the rest of the page, because there is nothing else
+// but in general, there could be things after here that we don't want them
+// to see.
