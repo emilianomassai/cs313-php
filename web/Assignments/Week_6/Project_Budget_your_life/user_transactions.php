e66d0061dd93f05b562ce8271f778838cf2abba1
Emiliano Massai <emilianomassai@icloud.com>
2020-10-23 17:23:10 +0100

    .

--- web/Assignments/Week_6/Project_Budget_your_life/user_transactions.php
+++ web/Assignments/Week_6/Project_Budget_your_life/user_transactions.php
@@ -44,6 +44,7 @@ foreach ($db->query('SELECT display_name, user_name, user_id, password FROM budg
         <table border="1" style="margin-left:auto;margin-right:auto" class="table table-bordered">
             <thead>
                 <tr>
+                    <th style="padding:10px">Transaction ID</th>
                     <th style="padding:10px">Amount</th>
                     <th style="padding:10px">Notes</th>
                     <th style="padding:10px">Category</th>
@@ -55,8 +56,9 @@ foreach ($db->query('SELECT display_name, user_name, user_id, password FROM budg
 
 
                 <?php $transaction_count = 0;?>
-                <?php foreach ($db->query('SELECT amount, user_id, notes, category, date FROM transaction') as $row) {
+                <?php foreach ($db->query('SELECT transaction_id, amount, user_id, notes, category, date FROM transaction') as $row) {
     $transactions_array[] = [
+        'transaction_id' => $row['transaction_id'],
         'amount' => $row['amount'],
         'user_id' => $row['user_id'],
         'notes' => $row['notes'],
@@ -67,6 +69,7 @@ foreach ($db->query('SELECT display_name, user_name, user_id, password FROM budg
 
     if (($_POST["user_transaction"] == $transactions_array[$transaction_count]['user_id'])) {?>
                 <tr>
+                    <td style="padding:10px"><?php echo $transactions_array[$transaction_count]['transaction_id'] ?>
                     <td style="padding:10px"><?php echo $transactions_array[$transaction_count]['amount'] ?>
                         <?php $totalAmount += $transactions_array[$transaction_count]['amount'];?>
                     </td>
