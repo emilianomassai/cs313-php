3e246a8b8d3da9d9aaf065917ea1bfe54bb923b4
Emiliano Massai <emilianomassai@icloud.com>
2020-10-23 19:54:20 +0100

    .

--- web/Assignments/Week_6/Project_Budget_your_life/modify_transaction.php
+++ web/Assignments/Week_6/Project_Budget_your_life/modify_transaction.php
@@ -11,7 +11,26 @@
 session_start();
 require "../Project_Budget_your_life/connectAppDB.php";
 $db = get_db();
+?>
 
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <link rel="stylesheet" href="style.css" />
+    <link rel="icon" type="image/ico" href="./BudgetAppImages/budgetAppIcon.png">
+    <script src="../Project_Budget_your_life/javaScript.js"></script>
+
+    <!--Title in the browser title bar.-->
+    <title>Budget Your Life</title>
+    <!-- heading of the web page -->
+</head>
+
+<body>
+    <h1>Modify Transaction </h1>
+    <p>In this page you can update the information of the selected transaction.</p>
+
+    <?php
 $editTransaction = $_POST['edit'];
 $_SESSION['transactions'] = $sessionTransactions;
 
@@ -29,19 +48,36 @@ foreach ($db->query('SELECT transaction_id, amount, user_id, notes, category, da
     $_SESSION['transactions'] = $transactions_array;
 
     if (($_POST["edit"] == $transactions_array[$transaction_count]['transaction_id'])) {?>
-<tr>
-    <td style="padding:10px"><?php echo $transactions_array[$transaction_count]['amount'] ?>
+    <tr>
+        <td style="padding:10px"><?php echo $transactions_array[$transaction_count]['amount'] ?>
 
-    </td>
-    <td style="padding:10px"><?php echo $transactions_array[$transaction_count]['notes'] ?>
-    </td>
-    <td style="padding:10px"><?php echo $transactions_array[$transaction_count]['category'] ?>
-    </td>
-    <td style="padding:10px"><?php echo $transactions_array[$transaction_count]['date'] ?>
+        </td>
+        <td style="padding:10px"><?php echo $transactions_array[$transaction_count]['notes'] ?>
+        </td>
+        <td style="padding:10px"><?php echo $transactions_array[$transaction_count]['category'] ?>
+        </td>
+        <td style="padding:10px"><?php echo $transactions_array[$transaction_count]['date'] ?>
 
-    </td>
-    <?php }?>
+        </td>
+        <?php }?>
 
-</tr>
-<?php $transaction_count++;
+    </tr>
+    <?php $transaction_count++;
 }?>
+
+
+
+    <div>
+        <a href="../Project_Budget_your_life/budgetApp.php" id="CS313_assignments_btn_id">
+            Go back to the App Homepage
+        </a>
+    </div>
+
+    <footer>
+        <p style="text-align: center;">
+            Copyright © <?php echo $today = date("Y"); ?> emiDev Inc. All rights reserved.
+        </p>
+    </footer>
+</body>
+
+</html>
