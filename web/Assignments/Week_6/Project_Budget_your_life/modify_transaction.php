25cad206100653f686ed9fb974215c87aa3af0d1
Emiliano Massai <emilianomassai@icloud.com>
2020-10-23 18:29:27 +0100

    .

--- web/Assignments/Week_6/Project_Budget_your_life/modify_transaction.php
+++ web/Assignments/Week_6/Project_Budget_your_life/modify_transaction.php
@@ -1,5 +1,9 @@
 <?php
+// start session
+session_start();
+require "../Project_Budget_your_life/connectAppDB.php";
+$db = get_db();
 
 $editTransaction = $_POST['edit'];
 
-echo 'Edit transaction: ' . $editTransaction;
+echo 'Edit transaction number: ' . $editTransaction;
