853d0c7fa13f79994ae271b0d4fa2664938a7404
Emiliano Massai <emilianomassai@icloud.com>
2020-10-23 15:07:21 +0100

    .

--- web/Assignments/Week_6/Project_Budget_your_life/javaScript.js
+++ web/Assignments/Week_6/Project_Budget_your_life/javaScript.js
@@ -34,6 +34,8 @@ function validateNewUserForm() {
 }
 
 function validateNewTransactionForm() {
+  var regexDateValidation = /^(((0[1-9]|[12]\d|3[01])\/(0[13578]|1[02])\/((19|[2-9]\d)\d{2}))|((0[1-9]|[12]\d|30)\/(0[13456789]|1[012])\/((19|[2-9]\d)\d{2}))|((0[1-9]|1\d|2[0-8])\/02\/((19|[2-9]\d)\d{2}))|(29\/02\/((1[6-9]|[2-9]\d)(0[48]|[2468][048]|[13579][26])|(([1][26]|[2468][048]|[3579][26])00))))$/g;
+
   var amountValidation =
     document.forms["newTransactionForm"]["input_amount"].value;
   var notesValidation =
@@ -50,10 +52,7 @@ function validateNewTransactionForm() {
     return false;
   }
 
-  if (
-    dateValidation == "" ||
-    /^\d{4}[\/\-](0?[1-9]|1[012])[\/\-](0?[1-9]|[12][0-9]|3[01])$/
-  ) {
+  if (dateValidation == "" || dateValidation != regexDateValidation) {
     alert("Please enter a date in the format 'YYYY-MM-DD'");
     return false;
   } else {
