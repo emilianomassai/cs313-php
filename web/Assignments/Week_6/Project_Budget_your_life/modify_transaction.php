c2fa357abf31c99e6cbcad64e843e5fa7b17de22
Emiliano Massai <emilianomassai@icloud.com>
2020-10-23 18:50:53 +0100

    .

--- web/Assignments/Week_6/Project_Budget_your_life/modify_transaction.php
+++ web/Assignments/Week_6/Project_Budget_your_life/modify_transaction.php
@@ -1,4 +1,12 @@
 <?php
+/**********************************************************
+ * File: modify_transaction.php
+ * Author: Emiliano Massai
+ *
+ * Description: This file use the transaction ID to retrieve
+ * all the information of a given transaction. The user can
+ * then modify or delete the transaction.
+ ***********************************************************/
 // start session
 session_start();
 require "../Project_Budget_your_life/connectAppDB.php";
@@ -7,3 +15,23 @@ $db = get_db();
 $editTransaction = $_POST['edit'];
 
 echo 'Edit transaction number: ' . $editTransaction;
+
+foreach ($db->query('SELECT transaction_id, amount, user_id, notes, category, date FROM transaction') as $row) {
+    $transactions_array[] = [
+        'transaction_id' => $row['transaction_id'],
+        'amount' => $row['amount'],
+        'user_id' => $row['user_id'],
+        'notes' => $row['notes'],
+        'category' => $row['category'],
+        'date' => $row['date'],
+    ];
+    if (($editTransaction == $transactions_array[$transaction_count]['transaction_id'])) {
+        echo 'Transaction number: ' . $editTransaction;
+        echo 'Amount: ' . $transactions_array[$transaction_count]['amount'];
+        echo 'User ID: ' . $transactions_array[$transaction_count]['user_id'];
+        echo 'Notes: ' . $transactions_array[$transaction_count]['notes'];
+        echo 'Category: ' . $transactions_array[$transaction_count]['category'];
+        echo 'Date: ' . $transactions_array[$transaction_count]['date'];
+
+    }
+}
