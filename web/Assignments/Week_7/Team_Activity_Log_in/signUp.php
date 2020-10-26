2713f5f3c3df7b635c4f01ac78f16294bf7656a6
Emiliano Massai <emilianomassai@icloud.com>
2020-10-26 11:05:19 +0000

    .

--- web/Assignments/Week_7/Team_Activity_Log_in/signUp.php
+++ web/Assignments/Week_7/Team_Activity_Log_in/signUp.php
@@ -1,62 +1,46 @@
 <?php
-/************************************************************
- * File: sign-up.php
- * Author: Emiliano Massai
+/**********************************************************
+ * File: singup.php
+ * Author: Br. Burton
  *
- * Description:
- * Sign-up page that asks for a username and password, and
- * then inserts this into the database.
+ * Description: Allows a user to enter a new username
+ *   and password to add to the DB.
+ *
+ * It posts to a file called "createAccount.php"
+ *   which does the actual creation.
+ *
  ***********************************************************/
 ?>
 
 <!DOCTYPE html>
-<html lang="en-US">
+<html>
 
 <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <!--Title in the browser title bar.-->
-    <title>Sign-up</title>
-    <!-- heading of the web page -->
-</head>
-
+    <title>Sign Up</title>
 </head>
 
 <body>
-    <h1>Please enter the following information:</h1>
-    <br>
-
-
-    <form class="newUserForm" name="newUserForm" action="sign_in.php" method="post"></form>
-
-    <h4><label for="username_txt"> Username:</label> </h4>
-    <input type="text" id="username" name="username">
-
-    <h4><label for="password_txt"> Password:</label> </h4>
-    <input type="password" id="password" name="password">
-
-
-    <br>
-    <br>
-
-    <div class="bottomBar">
-        <input type="submit" name="Add User" id="addUser">Add User</input>
-    </div>
-
-    </form>
-
     <div>
-        <a href="../../Home_Page/assignments.php" id="CS313_assignments_btn_id">
-            Go to the CS313 Assignment's page
-        </a>
+
+        <h1>Sign up for new account</h1>
+
+        <form id="mainForm" action="createAccount.php" method="POST">
+
+            <input type="text" id="txtUser" name="txtUser" placeholder="Username">
+            <label for="txtUser">Username</label>
+            <br /><br />
+
+            <input type="password" id="txtPassword" name="txtPassword" placeholder="Password"></input>
+            <label for="txtPassword">Password</label>
+            <br /><br />
+
+            <input type="submit" value="Create Account" />
+
+        </form>
+
+
     </div>
 
-    <footer>
-        <p style="text-align: center;">
-            Copyright © <?php echo $today = date("Y"); ?> emiDev Inc. All rights reserved.
-        </p>
-    </footer>
 </body>
 
 </html>
