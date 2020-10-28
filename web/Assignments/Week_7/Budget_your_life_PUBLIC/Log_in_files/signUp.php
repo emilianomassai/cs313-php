a054f31ea446e672c957d8cbaedce16958203cca
Emiliano Massai <emilianomassai@icloud.com>
2020-10-28 23:47:32 +0000

    .

--- web/Assignments/Week_7/Budget_your_life_PUBLIC/Log_in_files/signUp.php
+++ web/Assignments/Week_7/Budget_your_life_PUBLIC/Log_in_files/signUp.php
@@ -13,11 +13,21 @@
 ?>
 
 <!DOCTYPE html>
-<html>
+<html lang="en-US">
 
 <head>
-    <title>Sign Up</title>
-</head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <link rel="stylesheet" href="../style.css" />
+
+    <link rel="stylesheet" href="../javaScript.js" />
+    <link rel="icon" type="image/ico" href="../BudgetAppImages/budgetAppIcon.png">
+    <html>
+
+    <head>
+        <title>Budget Your Life - Sign Up</title>
+    </head>
 
 <body>
     <div>
