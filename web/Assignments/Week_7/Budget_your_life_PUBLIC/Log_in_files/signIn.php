9e4388cf8a364434a565863e87dc581c0c883771
Emiliano Massai <emilianomassai@icloud.com>
2020-10-28 23:32:42 +0000

    .

--- web/Assignments/Week_7/Budget_your_life_PUBLIC/Log_in_files/signIn.php
+++ web/Assignments/Week_7/Budget_your_life_PUBLIC/Log_in_files/signIn.php
@@ -64,6 +64,8 @@ if (isset($_POST['txtUser']) && isset($_POST['txtPassword'])) {
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <link rel="stylesheet" href="../style.css" />
+
     <link rel="stylesheet" href="../javaScript.js" />
     <link rel="icon" type="image/ico" href="../BudgetAppImages/budgetAppIcon.png">
     <html>
