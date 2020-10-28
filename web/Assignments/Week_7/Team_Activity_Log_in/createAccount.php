3869adf6e98ab2d94f8664e34a80c23afe675a9e
Emiliano Massai <emilianomassai@icloud.com>
2020-10-28 09:13:56 +0000

    .

--- web/Assignments/Week_7/Team_Activity_Log_in/createAccount.php
+++ web/Assignments/Week_7/Team_Activity_Log_in/createAccount.php
@@ -1,14 +1,14 @@
 <?php
 /**********************************************************
-* File: createAccount.php
-* Author: Br. Burton
-* 
-* Description: Accepts a new username and password on the
-*	POST variable, and creates it in the DB.
-*
-* The user is then redirected to the signIn.php page.
-*
-***********************************************************/
+ * File: createAccount.php
+ * Author: Br. Burton
+ *
+ * Description: Accepts a new username and password on the
+ *    POST variable, and creates it in the DB.
+ *
+ * The user is then redirected to the signIn.php page.
+ *
+ ***********************************************************/
 
 // If you have an earlier version of PHP (earlier than 5.5)
 // You need to download and include password.php.
@@ -19,10 +19,9 @@ $username = $_POST['txtUser'];
 $password = $_POST['txtPassword'];
 
 if (!isset($username) || $username == ""
-	|| !isset($password) || $password == "")
-{
-	header("Location: signUp.php");
-	die(); // we always include a die after redirects.
+    || !isset($password) || $password == "") {
+    header("Location: signUp.php");
+    die(); // we always include a die after redirects.
 }
 
 // Let's not allow HTML in our usernames. It would be best to also detect this before
@@ -33,7 +32,7 @@ $username = htmlspecialchars($username);
 $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
 
 // Connect to the database
-require("dbConnect.php");
+require "connectHerokuDB.php";
 $db = get_db();
 
 $query = 'INSERT INTO login(username, password) VALUES(:username, :password)';
@@ -47,12 +46,9 @@ $statement->bindValue(':password', $hashedPassword);
 
 $statement->execute();
 
-
 // finally, redirect them to the sign in page
 header("Location: signIn.php");
 die(); // we always include a die after redirects. In this case, there would be no
-       // harm if the user got the rest of the page, because there is nothing else
-       // but in general, there could be things after here that we don't want them
-       // to see.
-
-?>
+// harm if the user got the rest of the page, because there is nothing else
+// but in general, there could be things after here that we don't want them
+// to see.
