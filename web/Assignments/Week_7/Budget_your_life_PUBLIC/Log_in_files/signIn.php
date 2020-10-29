d294afa645b43ac39dbb4375697ae2766ca6ca98
Emiliano Massai <emilianomassai@icloud.com>
2020-10-29 10:31:00 +0000

    .

--- web/Assignments/Week_7/Budget_your_life_PUBLIC/Log_in_files/signIn.php
+++ web/Assignments/Week_7/Budget_your_life_PUBLIC/Log_in_files/signIn.php
@@ -83,7 +83,7 @@ if (isset($_POST['txtUser']) && isset($_POST['txtPassword'])) {
 
 
         <h1>Please sign in below:</h1>
-
+        <br>
         <?php
 if ($badLogin) {
     echo "<h4 style='color:rgba(250, 98, 98, 0.713)'>Incorrect Username or password!</h4>\n";
