671b972bc16b5cf2d1d1eb4e4285f500477e9533
Emiliano Massai <emilianomassai@icloud.com>
2020-10-29 20:28:44 +0000

    .

--- web/Assignments/Week_7/Budget_your_life_PUBLIC/Transactions/delete_transaction.php
+++ web/Assignments/Week_7/Budget_your_life_PUBLIC/Transactions/delete_transaction.php
@@ -96,7 +96,7 @@ $_SESSION['transactions'] = $sessionTransactions;
             <br>
 
             <div class="bottomBar">
-                <button type="submit" name="Delete Transaction" id="deleteTransaction">Delete
+                <button tyle="width: 200px" type="submit" name="Delete Transaction" id="deleteTransaction">Delete
                     Transaction</button>
             </div>
 
