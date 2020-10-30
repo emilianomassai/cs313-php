1c81b15fff439cc3e4fac3819d6e31c596dc5b8d
Emiliano Massai <emilianomassai@icloud.com>
2020-10-30 00:39:20 +0000

    .

--- web/Assignments/Week_7/Budget_your_life_PUBLIC/Transactions/user_transactions.php
+++ web/Assignments/Week_7/Budget_your_life_PUBLIC/Transactions/user_transactions.php
@@ -109,14 +109,14 @@ $transaction_count = 0;
 
     <br>
     <div class="bottomBar">
-        <input style="width: 200px" type="submit" name="Delete Transaction" id="deleteTransaction"
-            value="Delete Transaction">
-        </input>
-        <br>
-        <br>
 
         <input style="width: 200px" type="submit" name="Modify Transaction" value="Modify Transaction"
             id="modifyTransaction" formaction="../Transactions/modify_transaction.php"> </input>
+        <br>
+        <br>
+        <input style="width: 200px" type="submit" name="Delete Transaction" id="deleteTransaction"
+            value="Delete Transaction">
+        </input>
     </div>
 
     </form>
