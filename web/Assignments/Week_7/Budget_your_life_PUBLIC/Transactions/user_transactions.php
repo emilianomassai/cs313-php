6079b701c4a1d74b325fb4b5d792399d1a96bfab
Emiliano Massai <emilianomassai@icloud.com>
2020-10-30 11:42:26 +0000

    .

--- web/Assignments/Week_7/Budget_your_life_PUBLIC/Transactions/user_transactions.php
+++ web/Assignments/Week_7/Budget_your_life_PUBLIC/Transactions/user_transactions.php
@@ -42,11 +42,21 @@ $currentDisplayName = $_SESSION['current_display_name'];
         <table border="1" style="margin-left:auto;margin-right:auto" class="table table-bordered">
             <thead>
                 <tr>
-                    <th style="padding:10px">Amount</th>
-                    <th style="padding:10px">Notes</th>
-                    <th style="padding:10px">Category</th>
-                    <th style="padding:10px">Date</th>
-                    <th style="padding:10px">Select to edit</th>
+                    <th style="padding:10px">
+                        <h4>Amount</h4>
+                    </th>
+                    <th style="padding:10px">
+                        <h4>Notes</h4>
+                    </th>
+                    <th style="padding:10px">
+                        <h4>Category</h4>
+                    </th>
+                    <th style="padding:10px">
+                        <h4>Date</h4>
+                    </th>
+                    <th style="padding:10px">
+                        <h4>Select to edit</h4>
+                    </th>
                 </tr>
             </thead>
             <tbody>
