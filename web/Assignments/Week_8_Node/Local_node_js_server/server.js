bbc54fb125f6bf60668685b38099bb458ff60040
Emiliano Massai <emilianomassai@icloud.com>
2020-11-06 17:10:58 +0000

    assignment server completed

--- web/Assignments/Week_8_Node/Local_node_js_server/server.js
+++ web/Assignments/Week_8_Node/Local_node_js_server/server.js
@@ -5,8 +5,8 @@ var http = require("http");
 // in this case there is a request and a response from and to the server.
 function sayHello(req, res) {
   // console.log("Hello World");
-  console.log("Recieved a request for: " + req.url);
-  console.log("Starting server status: " + res.statusCode);
+  console.log("Received a request for: " + req.url);
+  console.log("The initial server status is: " + res.statusCode);
 
   if (req.url == "/home.html") {
     console.log("Server status when /home.html: " + res.statusCode);
@@ -14,8 +14,42 @@ function sayHello(req, res) {
     res.write("<h1>Welcome to the Home Page</h1>");
     res.end();
   } else if (req.url == "/getData") {
-    res.write("<h1>Got some data</h1>");
-    res.end();
+    const fs = require("fs");
+
+    fs.readFile(
+      "/Volumes/GoogleDrive/My Drive/BYU-I/Classes/7.CSE 341_Web Engineering II/GitHub_remote/cs313-php/web/Assignments/Week_8_Node/Local_node_js_server/student.json",
+      "utf8",
+      (err, data) => {
+        if (err) {
+          console.log(`Error reading file from disk: ${err}`);
+        } else {
+          // parse JSON string to JSON object
+          const students = JSON.parse(data);
+          console.log(students);
+
+          // print all databases
+          students.forEach((student) => {
+            res.write(
+              `My name is ${student.name} and I'm ${student.age} years old.` +
+                "\n" +
+                `I'm a ${student.profession} and I live in ${student.city}. I have a ${student.car} as my car.` +
+                "\n" +
+                `I'm studing ${student.major} and I'm in the ${student.class} class.` +
+                "\n" +
+                "\n"
+            );
+          });
+          // res.write(
+          //   `My name is ${students.name} and I'm ${students.age} years old. I'm a ${students.profession} and I live in ${students.city}. I have a ${students.car} as my car. I'm studing ${students.major} and I'm in the ${students.class} class.`
+          // );
+
+          res.end();
+        }
+      }
+    );
+    // res.writeHead(200, { "Content-Type": "application/json" });
+
+    // res.end();
   } else {
     res.statusCode = 404;
     console.log("Error from server " + res.statusCode);
