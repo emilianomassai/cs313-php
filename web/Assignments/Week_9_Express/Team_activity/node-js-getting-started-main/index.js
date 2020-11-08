abaeef1c10f3614e630d4274ddf283fb356021ed
Emiliano Massai <emilianomassai@icloud.com>
2020-11-08 10:52:16 +0000

    .

--- web/Assignments/Week_9_Express/Team_activity/node-js-getting-started-main/index.js
+++ web/Assignments/Week_9_Express/Team_activity/node-js-getting-started-main/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const path = require("path");
-const PORT = process.env.PORT || 5000;
+const app = express();
+const port = process.env.PORT || 5000;
 
 express()
   // tell it to use the public directory as one where static files live
